45d31b5a1d111e2130cb163f460ac8e17e56e63f
Lily Paczesniak <lilypaczesniak@outlook.com>
2021-12-06 10:26:23 +0000

    pass isRemove to componennts

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -23,6 +23,7 @@ class App extends React.Component {
         ]
       };
       this.addTrack = this.addTrack.bind(this);
+      this.removeTrack = this.addTrack.bind(this);
   }
 
   addTrack(track) {
@@ -52,7 +53,8 @@ class App extends React.Component {
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults}
             onAdd={this.addTrack}/>
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
+            <Playlist playlistName={this.state.playlistName} 
+            playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack}/>
           </div>
         </div>
       </div>
