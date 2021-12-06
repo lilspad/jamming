f8063fa2427c679c525ba87a32deee4282dc04a8
Lily Paczesniak <lilypaczesniak@outlook.com>
2021-12-06 11:21:24 +0000

    savePlaylist method

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -6,7 +6,7 @@ class Playlist extends React.Component {
     constructor(props) {
         super(props);
 
-        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this); 
     }
 
     handleNameChange(event) {
@@ -20,7 +20,8 @@ class Playlist extends React.Component {
                 onChange={this.handleNameChange}/>
                 <TrackList tracks={this.props.playlistTracks}
                 onRemove={this.props.onRemove} isRemoval={true}/>
-                <button className="Playlist-save">SAVE TO SPOTIFY</button>
+                <button className="Playlist-save" onClick={this.props.onSave}
+                >SAVE TO SPOTIFY</button>
             </div>
         )
     }
