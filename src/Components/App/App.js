7fbeaeba2718224ded73eaaf3d5cf307df874b40
Lily Paczesniak <lilypaczesniak@outlook.com>
2021-12-06 10:54:30 +0000

    change playlist name

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -23,7 +23,8 @@ class App extends React.Component {
         ]
       };
       this.addTrack = this.addTrack.bind(this);
-      this.removeTrack = this.addTrack.bind(this);
+      this.removeTrack = this.removeTrack.bind(this);
+      this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -43,6 +44,10 @@ class App extends React.Component {
     this.setState({playlistTracks: tracks});
   }
 
+  updatePlaylistName(name) {
+    this.setState({playlistName: name});
+  }
+
   render() {
     return (
       
@@ -54,7 +59,8 @@ class App extends React.Component {
             <SearchResults searchResults={this.state.searchResults}
             onAdd={this.addTrack}/>
             <Playlist playlistName={this.state.playlistName} 
-            playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack}/>
+            playlistTracks={this.state.playlistTracks} 
+            onRemove={this.removeTrack} onNameChange={this.updatePlaylistName}/>
           </div>
         </div>
       </div>
